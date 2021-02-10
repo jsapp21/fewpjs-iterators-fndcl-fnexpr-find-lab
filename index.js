5bbb3225714f472cb774e523e02d0783f7722854
James Sapp <jameseisapp@gmail.com>
2021-02-09 21:33:13 -0600

    commit tree 8127ceb82f250dead035d5e2c9daf8db2773aeef

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+const superbowlWin = (record) => {
+  let superBowlDate = record.find(element => element.result === "W")
+  debugger
+  
+  return !!superBowlDate ? superBowlDate.year : undefined
 }
+
+
+superbowlWin(superBowlDate)
